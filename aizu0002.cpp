642e82a5db5e8b9b9c5b041b8cb5bd462f825907
SyoutarouTakagi <shotaro.takagi@befool.co.jp>
2014-11-05 18:35:33 +0900

    first commit

---
+++ aizu0002.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int sum;
+    while(cin >> a >> b)
+    {
+        string sum = to_string(a + b);
+        cout << sum.length() << endl;
+    }
+}
